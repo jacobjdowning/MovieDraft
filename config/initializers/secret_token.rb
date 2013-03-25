fd547a83c348d0dca0e4ea6b2b91ccc3c93d6dff
Jacob Downing <jacobjdowning@gmail.com>
2013-03-24 22:01:54 -0400

    initial commit, empty ruby on rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieDraft::Application.config.secret_token = 'ad2e72110740b77dade6b7874f96cfaf1508ed7030245785a39e7813cec9661f9f478e35c5809a5c470d88ed5fa60682f2725468cd00ca46df8fe806cd68e7d6'
